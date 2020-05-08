adc80d525b9866f0200d35bc2d71e4a87829cf71
Gamze Ceylan <cceylan.5445@gmail.com>
2020-05-08 23:57:04 +0300

    web-teknolojileri-proje

---
+++ Send.php
@@ -0,0 +1,10 @@
+<?php 
+    echo "Adı : ".$_POST['ad'];
+    echo "<br>Soyadı : ".$_POST['soyad'];
+    echo "<br>Doğum Tarihi : ".$_POST['dogum'];
+    echo "<br>Eğitim Düzeyi : ".$_POST['egitim'];
+    echo "<br>E-Mail : ".$_POST['email'];
+    echo "<br>Cinsiyet : ".$_POST['cns'];
+    echo "<br>Kullanıcı Mesajı : ".$_POST['mesaj'];
+    echo "<br>Gönderilen Dosya : ".$_POST['dosya'];
+?>
